42ce1a3fe6f7d472bec46841f89a0f9c55b101e9
nandanathaniela <natnanda04@gmail.com>
2023-09-20 10:39:13 +0700

    tugas 3 update

--- main/views.py
+++ main/views.py
@@ -13,7 +13,8 @@ def show_main(request):
     context = {
         'name': 'Nanda Nathaniela Meizari',
         'class': 'PBP F',
-        'products': products
+        'products': products,
+        'total' : products.__len__(),
     }
 
     return render(request, "main.html", context)
